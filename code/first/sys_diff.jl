a66f57023885a55bbc6e7daf919c7be938280d81
n <2585803+tt5@users.noreply.github.com>
2026-01-22 03:40:59 +0100

    function name

--- code/first/sys_diff.jl
+++ code/first/sys_diff.jl
@@ -3,7 +3,7 @@ import DifferentialEquations as DE
 import Plots
 using Plots; plotlyjs()
 
-function lorenz!(du,u,p,t)
+function drei!(du,u,p,t)
     a,b,c,d,e,f,g,h,i = p
     du[1] = u[1] - a*cos(u[1])*u[1] - b*cos(u[3])*u[3] - c*cos(u[2])*u[2]
     du[2] = u[2] - d*cos(u[2])*u[2] - e*cos(u[1])*u[1] - f*cos(u[3])*u[3]
@@ -14,7 +14,7 @@ u0 = [1.0,1.0,1.0]
 p = [0.1,0.1,0.1,0.2,0.2,0.2,0.4,0.4,0.4]
 
 tspan = (0.0, 80.0)
-prob = DE.ODEProblem(lorenz!,u0,tspan,p)
+prob = DE.ODEProblem(drei!,u0,tspan,p)
 
 sol = DE.solve(prob)
 
