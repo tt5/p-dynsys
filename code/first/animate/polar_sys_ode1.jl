f5f196c52a307a5d12c2c567cc594fd1b98b017b
n <2585803+tt5@users.noreply.github.com>
2026-01-24 03:02:26 +0100

    animated r

---
+++ code/first/animate/polar_sys_ode1.jl
@@ -0,0 +1,26 @@
+import DifferentialEquations as DE
+
+import Plots
+using Plots; plotlyjs()
+using Printf
+
+function bifurk!(du,u,p,t)
+    a,b,c = p
+    du[1] = (1 + b*u[2]^2)
+    du[2] = c*u[2] + a*u[2]^5
+end
+
+
+
+for i in 0:200
+  tspan = (0.0, 200.0)
+  p = [0.001,1,-0.1]
+  u0 = [-200,2.0+i/100]
+  prob = DE.ODEProblem(bifurk!,u0,tspan,p)
+  sol = DE.solve(prob)
+  p = plot(sol, idxs=(1,2), denseplot=false,
+        xlimits=(-200,400),
+        ylimits=(0,10)
+          )
+  savefig(p, "$(@sprintf("%04d",i)).png")
+end
